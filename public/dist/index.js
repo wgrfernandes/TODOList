969eae07842b1f02cb02442675ccf2e6f1483816
Holdrulff <wesley.fernan1@gmail.com>
2023-03-12 23:21:44 -0300

    update

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,2 +1,36 @@
 "use strict";
-alert("Início do projeto em TS");
+(function () {
+    var todo = {
+        description: "todo",
+        done: "false"
+    };
+    var reminder = {
+        description: "reminder",
+        date: "09.03.2023"
+    };
+    var taskView = {
+        render: function (tasks) {
+            var tasksList = document.getElementById("tasksList");
+            while (tasksList === null || tasksList === void 0 ? void 0 : tasksList.firstChild) {
+                tasksList.removeChild(tasksList.firstChild);
+            }
+            tasks.forEach(function (task) {
+                var li = document.createElement("LI");
+                var textNode = document.createTextNode(JSON.stringify(task));
+                li.appendChild(textNode);
+                tasksList === null || tasksList === void 0 ? void 0 : tasksList.appendChild(li);
+            });
+        }
+    };
+    var TaskController = function (view) {
+        var _a;
+        var tasks = [todo, reminder];
+        var handleEvent = function (event) {
+            event.preventDefault();
+            view.render(tasks);
+        };
+        (_a = document
+            .getElementById("taskForm")) === null || _a === void 0 ? void 0 : _a.addEventListener("submit", handleEvent);
+    };
+    TaskController(taskView);
+})();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,135 @@
-alert("Início do projeto em TS");
+// IIFE to preserve code scope
+(() => {
+  // Enums
+  enum NotificationPlatform {
+    SMS = "SMS",
+    EMAIL = "EMAIL",
+    PUSH_NOTIFICATION = "PUSH_NOTIFICATION",
+  }
+
+  // Utils
+  const DateUtils = {
+    tomorrow(): Date {
+      const tomorrow = new Date();
+      tomorrow.setDate(tomorrow.getDate() + 1);
+      return tomorrow;
+    },
+    today(): Date {
+      return new Date();
+    },
+    formatDate(date: Date): string {
+      return `${date.getDate()}.${date.getMonth() + 1}.${date.getFullYear()}`;
+    },
+  };
+
+  const UUID = (): string => {
+    return Math.random().toString(36).substr(2, 9);
+  };
+
+  // Interfaces
+  interface Task {
+    id: string;
+    dateCreated: Date;
+    dateUpdated: Date;
+    description: string;
+    render(): string;
+  }
+
+  class Reminder implements Task {
+    id: string = UUID();
+    dateCreated: Date = DateUtils.today();
+    dateUpdated: Date = DateUtils.today();
+    description: string = "";
+
+    scheduleDate: Date = DateUtils.tomorrow();
+    notifications: Array<NotificationPlatform> = [NotificationPlatform.EMAIL];
+
+    constructor(
+      description: string,
+      scheduleDate: Date,
+      notifications: Array<NotificationPlatform>
+    ) {
+      this.description = description;
+      this.scheduleDate = scheduleDate;
+      this.notifications = notifications;
+    }
+
+    render(): string {
+      return `
+      ---> Reminder <--- \n
+      Description: ${this.description}\n
+      Notify by ${this.notifications.join("and")} in ${DateUtils.formatDate(
+        this.scheduleDate
+      )}\n
+      Created: ${DateUtils.formatDate(
+        this.dateCreated
+      )} Last Update: ${DateUtils.formatDate(this.dateUpdated)}      
+      `;
+    }
+  }
+
+  class Todo implements Task {
+    id: string = UUID();
+    dateCreated: Date = DateUtils.today();
+    dateUpdated: Date = DateUtils.today();
+    description: string = "";
+
+    done: boolean = false;
+
+    constructor(description: string) {
+      this.description = description;
+    }
+
+    render(): string {
+      const doneLabel = this.done ? "Completed" : "In Progress";
+      return `
+      ---> To do <--- \n
+      Description: ${this.description}\n
+      Status: ${doneLabel}\n
+      Created: ${DateUtils.formatDate(this.dateCreated)}\n
+      Last Update: ${DateUtils.formatDate(this.dateUpdated)} `;
+    }
+  }
+
+  // Mocks
+  const todoMock: Todo = new Todo("Help mark with code testing");
+
+  const reminderMock: Reminder = new Reminder("My brother's bday", new Date(), [
+    NotificationPlatform.EMAIL,
+  ]);
+
+  const taskView = {
+    render(tasks: Array<Task>) {
+      //Clear view
+      const tasksList = document.getElementById("tasksList");
+      while (tasksList?.firstChild) {
+        tasksList.removeChild(tasksList.firstChild);
+      }
+      //Render Tasks
+      //task.forEach((task) => console.log(JSON.stringfy(task)));
+
+      tasks.forEach((task) => {
+        const li = document.createElement("LI");
+        const textNode = document.createTextNode(task.render());
+        li.appendChild(textNode);
+        tasksList?.appendChild(li);
+      });
+    },
+  };
+
+  // Controllers
+  const TaskController = (view: typeof taskView) => {
+    const tasks: Array<Task> = [todoMock, reminderMock];
+
+    const handleTaskCreate = (event: Event) => {
+      event.preventDefault();
+      view.render(tasks);
+    };
+
+    document
+      .getElementById("taskForm")
+      ?.addEventListener("submit", handleTaskCreate);
+  };
+
+  TaskController(taskView);
+})();
